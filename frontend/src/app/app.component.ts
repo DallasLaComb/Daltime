35c8ab1ef05693fc26f5241d70ac2d1fab16cc74
dallaslacomb <dallaslacombdrive@gmail.com>
2025-05-12 21:49:40 -0400

    Fixed some configuration so that I can use module.

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { provideRouter } from '@angular/router';
+import { routes } from './app.routes'; // your routes file
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'frontend';
