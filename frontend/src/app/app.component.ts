b7558c54465dd5452e86bba5a1be5f09b10a650d
dallaslacomb <dallaslacombdrive@gmail.com>
2025-05-12 22:51:36 -0400

    Imported Bootstrap

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,12 +1,8 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { provideRouter } from '@angular/router';
-import { routes } from './app.routes'; // your routes file
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
