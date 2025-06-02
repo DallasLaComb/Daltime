928507946df16c27f82f1b66cbf8e280a080bd5e
DallasLaComb <dallaslacombdrive@gmail.com>
2025-06-02 18:28:37 -0400

    refactor: update registration and login components to accept full payloads and improve error handling

--- frontend/src/app/auth/login/login.component.ts
+++ frontend/src/app/auth/login/login.component.ts
@@ -1,33 +1,39 @@
 // src/app/auth/login/login.component.ts
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
 import { AuthService } from '../auth.service';
+import { FormsModule } from '@angular/forms';
+import { InputComponent } from '@CommonShiftScheduler/form/input/input.component';
+import { ButtonComponent } from '@CommonShiftScheduler/ui/button/button.component';
+import { NgIf } from '@angular/common';
 
 @Component({
-  selector: 'app-login',
+  selector: 'shift-scheduler-login',
   standalone: true,
-  imports: [CommonModule, FormsModule, HttpClientModule],
   templateUrl: './login.component.html',
+  imports: [FormsModule, InputComponent, ButtonComponent, NgIf, RouterModule],
 })
 export class LoginComponent {
   email = '';
   password = '';
-  error = '';
+  error: string | null = null;
+  isLoading = false;
 
-  constructor(private authService: AuthService, private router: Router) {}
+  private auth = inject(AuthService);
+  private router = inject(Router);
 
-  onSubmit(): void {
-    this.authService.login(this.email, this.password).subscribe({
+  onSubmit() {
+    this.error = null;
+    this.isLoading = true;
+    this.auth.login(this.email, this.password).subscribe({
       next: (res) => {
-        console.log('SAVING AUTH DATA', res);
-        this.authService.saveAuthData(res);
-        this.router.navigate(['/employee/home']);
+        this.isLoading = false;
+        this.auth.saveAuthData(res);
+        this.router.navigate(['/']);
       },
       error: (err) => {
-        this.error = err.error?.error || 'Login failed. Please try again.';
+        this.isLoading = false;
+        this.error = err?.error?.error || 'Login failed. Please try again.';
       },
     });
   }
